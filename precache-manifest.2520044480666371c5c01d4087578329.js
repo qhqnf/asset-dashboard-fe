9b7e04391f17a1ada3dd28f0f1349a93a30f893a
root <didguscjf95@naver.com>
2020-08-18 21:02:02 +0900

    Updates

---
+++ precache-manifest.2520044480666371c5c01d4087578329.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3c917a09aa89ea884aa84d99ff3821",
+    "url": "/asset-dashboard-fe/index.html"
+  },
+  {
+    "revision": "5027d15c4ca5d73be907",
+    "url": "/asset-dashboard-fe/static/js/2.07705828.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/asset-dashboard-fe/static/js/2.07705828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68eb3560abf387dd8470",
+    "url": "/asset-dashboard-fe/static/js/main.affa8b13.chunk.js"
+  },
+  {
+    "revision": "8ff794280e48c78d37cd",
+    "url": "/asset-dashboard-fe/static/js/runtime-main.99e829df.js"
+  }
+]);
